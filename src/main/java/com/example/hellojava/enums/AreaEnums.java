827793a323337eeac21b9c1b6fefe776187c6212
ooobin <132541834+ooobin@users.noreply.github.com>
2024-04-01 09:27:47 +0800

    Rename enums

--- src/main/java/com/example/hellojava/enums/AreaEnums.java
+++ src/main/java/com/example/hellojava/enums/AreaEnums.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum PostEnum {
+public enum AreaEnums {
     /**
      * 济南
      */
@@ -26,7 +26,7 @@ public enum PostEnum {
      * @return area
      */
     public static String getArea(int code) {
-        for (PostEnum value : PostEnum.values()) {
+        for (AreaEnums value : AreaEnums.values()) {
             if (value.getCode() == code) {
                 return value.getArea();
             }
@@ -35,6 +35,6 @@ public enum PostEnum {
     }
 
     public static void main(String[] args) {
-        System.out.println(PostEnum.getArea(250000));
+        System.out.println(AreaEnums.getArea(250000));
     }
 }
