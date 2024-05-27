e0c0c007ab74ae56ded7a8ad8e3c8f7ed1e70feb
ooobin <132541834+ooobin@users.noreply.github.com>
2024-05-27 10:25:55 +0800

    update

---
+++ src/main/java/com/example/hellojava/base/ExcHandler.java
@@ -0,0 +1,28 @@
+package com.example.hellojava.base;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExcHandler {
+
+    @ExceptionHandler(Exception.class)
+    private static Result<String> handleException(Exception e) {
+        // Log the exception
+        logException(e);
+
+        return Result.failure(e.toString());
+    }
+
+    private static void logException(Exception e) {
+//        e.printStackTrace();
+        // Log the exception message and stack trace
+        log.error("An exception occurred: ");
+        log.error(ExceptionUtils.getMessage(e));
+        log.error(ExceptionUtils.getStackTrace(e));
+    }
+
+}
