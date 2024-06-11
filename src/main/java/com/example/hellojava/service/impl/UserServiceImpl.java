dac49e82f82d8b5fa76b4dd9bd6dbc4c6cd48e71
ooobin <132541834+ooobin@users.noreply.github.com>
2024-06-11 08:52:36 +0800

    update

--- src/main/java/com/example/hellojava/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/hellojava/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.hellojava.service.UserService;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,6 +21,9 @@ import java.util.UUID;
 @Transactional
 public class UserServiceImpl implements UserService {
 
+    @Value("${server.port}")
+    private int SERVER_PORT;
+
     private final UserMapper userMapper;
 
     @Autowired
@@ -33,6 +37,7 @@ public class UserServiceImpl implements UserService {
         HashMap<Object, Object> result = new HashMap<>();
         result.put("users", allUsers);
         result.put("status", 200);
+        System.out.println("SERVER_PORT: " + SERVER_PORT);
         return result;
     }
 
